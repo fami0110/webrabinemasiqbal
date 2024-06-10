b4f58d01a4f7b757fa66bd898b9db28c91b2ee89
fami0110 <masandofami@gmail.com>
2024-06-10 12:46:36 +0700

    update navbar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // Get url data query
 let url = window.location.href;
 let query = url.split('?', 2)[1];
-
 let data = {};
 if (query) {
+    if (query.includes('#')) query = query.split('#', 2)[0];
     query.split('&').forEach(item => {
         let split = item.split('=');
         data[split[0]] = split[1];
@@ -14,8 +14,37 @@ if (query) {
 let recipient = data['kepada'] || '';
 $('#recipient').text(decodeURIComponent(recipient));
 
+// Audio element handler
+let audio = $('audio')[0];
+
 // open cover event
-$('#open').on('click', () => {
+$('#open').on('click', function() {
     $('header').addClass('open');
-    $('audio')[0].play();
+    audio.volume = 0.5;
+    audio.play();
 });
+
+// Toogle event handler
+let autoscroll = false;
+$('#autoscroll').on('click', function() {
+    let btn = $(this);
+    btn.toggleClass('active');
+    if (btn.hasClass('active')) {
+        autoscroll = true;
+        start_autoscroll();
+    } else {
+        autoscroll = false;
+    }
+});
+
+$('#sound').on('click', function() {
+    let btn = $(this);
+    btn.toggleClass('active');
+    if (btn.hasClass('active')) audio.play();
+    else audio.pause();
+});
+
+function start_autoscroll() {
+    window.scrollBy(0, 2);
+    if (autoscroll) setTimeout(start_autoscroll, 10);
+}
